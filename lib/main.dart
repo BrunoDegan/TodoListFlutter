3d4e16eff098fcafd267e985bed89cce8a79188c
Bruno Degan <bmdegan@gmail.com>
2020-12-06 01:26:48 -0400

    Adding onRefresh method

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,8 @@ class _HomeState extends State<Home> {
 
   List _todoList = [];
   final _todoController = TextEditingController();
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
 
 
   @override
@@ -44,6 +46,25 @@ class _HomeState extends State<Home> {
       _todoList.add(newTodo);
       _saveData();
     });
+  }
+
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds:1));
+    setState(() {
+      _todoList.sort((a,b){
+        if(a["ok"] && !b["ok"])
+          return 1;
+        else if(!a["ok"] && b["ok"])
+          return -1;
+        else
+          return 0;
+      });
+
+      _saveData();
+
+    });
+
+    return null;
   }
 
   @override
@@ -81,10 +102,13 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-                padding: EdgeInsets.only(top: 10.0),
-                itemCount: _todoList.length,
-                itemBuilder: buildItem),
+            child: RefreshIndicator(
+              onRefresh: _refresh,
+              child: ListView.builder(
+                  padding: EdgeInsets.only(top: 10.0),
+                  itemCount: _todoList.length,
+                  itemBuilder: buildItem),
+            )
           )
         ],
       ),
@@ -102,7 +126,7 @@ class _HomeState extends State<Home> {
     return file.writeAsString(data);
   }
 
-  Future<String>  _readData() async {
+  Future<String> _readData() async {
     try {
       final file = await _getFile();
       return file.readAsString();
@@ -111,7 +135,7 @@ class _HomeState extends State<Home> {
     }
   }
 
- Widget buildItem(context,index) {
+ Widget buildItem(BuildContext context,int index) {
     return Dismissible(
       key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
       background: Container(
@@ -136,9 +160,35 @@ class _HomeState extends State<Home> {
             _saveData();
           });
         },
-      )
+      ), 
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_todoList[index]);
+          _lastRemovedPos = index;
+          _todoList.removeAt(index);
+          _saveData();
+
+          final SnackBar snack = SnackBar(
+            content: Text("Tarefa \" ${_lastRemoved["title"]} \" removida "),
+            action: SnackBarAction(label: "Desfazer",
+            onPressed: () {
+              setState(() {
+                _todoList.insert(_lastRemovedPos, _lastRemoved);
+                _saveData();
+              });
+            }),
+
+            duration: Duration (seconds: 2),
+          );
+
+          Scaffold.of(context).removeCurrentSnackBar();
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
     );
   }
+
+
 
 }
 
