88b82a3834f2975aa31b21746926876f91556a49
Bruno Degan <bmdegan@gmail.com>
2020-12-06 01:29:25 -0400

    Fixing some whitespaces

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
-
 void main() {
   runApp(MaterialApp(
     home: Home(),
@@ -13,18 +12,16 @@ void main() {
 
 class Home extends StatefulWidget {
   @override
- _HomeState createState() {
+  _HomeState createState() {
     return _HomeState();
   }
 }
 
 class _HomeState extends State<Home> {
-
   List _todoList = [];
   final _todoController = TextEditingController();
   Map<String, dynamic> _lastRemoved;
   int _lastRemovedPos;
-
 
   @override
   void initState() {
@@ -39,7 +36,7 @@ class _HomeState extends State<Home> {
 
   void _addTodo() {
     setState(() {
-      Map<String,dynamic> newTodo = Map();
+      Map<String, dynamic> newTodo = Map();
       newTodo["title"] = _todoController.text;
       _todoController.text = "";
       newTodo["ok"] = false;
@@ -49,19 +46,18 @@ class _HomeState extends State<Home> {
   }
 
   Future<Null> _refresh() async {
-    await Future.delayed(Duration(seconds:1));
+    await Future.delayed(Duration(seconds: 1));
     setState(() {
-      _todoList.sort((a,b){
-        if(a["ok"] && !b["ok"])
+      _todoList.sort((a, b) {
+        if (a["ok"] && !b["ok"])
           return 1;
-        else if(!a["ok"] && b["ok"])
+        else if (!a["ok"] && b["ok"])
           return -1;
         else
           return 0;
       });
 
       _saveData();
-
     });
 
     return null;
@@ -75,7 +71,6 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.blueAccent,
         centerTitle: true,
       ),
-
       body: Column(
         children: <Widget>[
           Container(
@@ -87,35 +82,32 @@ class _HomeState extends State<Home> {
                     controller: _todoController,
                     decoration: InputDecoration(
                         labelText: "Nova tarefa",
-                        labelStyle: TextStyle(color : Colors.blueAccent)
-                    ),
+                        labelStyle: TextStyle(color: Colors.blueAccent)),
                   ),
                 ),
-
                 RaisedButton(
                   color: Colors.blueAccent,
-                  child:Text("Add"),
+                  child: Text("Add"),
                   textColor: Colors.white,
-                  onPressed:_addTodo,
+                  onPressed: _addTodo,
                 )
               ],
             ),
           ),
           Expanded(
-            child: RefreshIndicator(
-              onRefresh: _refresh,
-              child: ListView.builder(
-                  padding: EdgeInsets.only(top: 10.0),
-                  itemCount: _todoList.length,
-                  itemBuilder: buildItem),
-            )
-          )
+              child: RefreshIndicator(
+            onRefresh: _refresh,
+            child: ListView.builder(
+                padding: EdgeInsets.only(top: 10.0),
+                itemCount: _todoList.length,
+                itemBuilder: buildItem),
+          ))
         ],
       ),
     );
   }
 
-  Future <File> _getFile() async {
+  Future<File> _getFile() async {
     final directory = await getApplicationDocumentsDirectory();
     return File("${directory.path}/data.json");
   }
@@ -130,37 +122,34 @@ class _HomeState extends State<Home> {
     try {
       final file = await _getFile();
       return file.readAsString();
-    } catch(e) {
+    } catch (e) {
       return null;
     }
   }
 
- Widget buildItem(BuildContext context,int index) {
+  Widget buildItem(BuildContext context, int index) {
     return Dismissible(
       key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
       background: Container(
-        color: Colors.red,
-        child: Align(
-          alignment: Alignment(-0.9, 0.0),
-          child: Icon(Icons.delete, color: Colors.white),
-
-        )
-      ),
+          color: Colors.red,
+          child: Align(
+            alignment: Alignment(-0.9, 0.0),
+            child: Icon(Icons.delete, color: Colors.white),
+          )),
       direction: DismissDirection.startToEnd,
-      child:   CheckboxListTile(
+      child: CheckboxListTile(
         title: Text(_todoList[index]['title']),
         value: _todoList[index]['ok'],
         secondary: CircleAvatar(
-          child: Icon(_todoList[index]['ok'] ?
-          Icons.check : Icons.error),
+          child: Icon(_todoList[index]['ok'] ? Icons.check : Icons.error),
         ),
-        onChanged: (checked){
+        onChanged: (checked) {
           setState(() {
             _todoList[index]["ok"] = checked;
             _saveData();
           });
         },
-      ), 
+      ),
       onDismissed: (direction) {
         setState(() {
           _lastRemoved = Map.from(_todoList[index]);
@@ -170,15 +159,15 @@ class _HomeState extends State<Home> {
 
           final SnackBar snack = SnackBar(
             content: Text("Tarefa \" ${_lastRemoved["title"]} \" removida "),
-            action: SnackBarAction(label: "Desfazer",
-            onPressed: () {
-              setState(() {
-                _todoList.insert(_lastRemovedPos, _lastRemoved);
-                _saveData();
-              });
-            }),
-
-            duration: Duration (seconds: 2),
+            action: SnackBarAction(
+                label: "Desfazer",
+                onPressed: () {
+                  setState(() {
+                    _todoList.insert(_lastRemovedPos, _lastRemoved);
+                    _saveData();
+                  });
+                }),
+            duration: Duration(seconds: 2),
           );
 
           Scaffold.of(context).removeCurrentSnackBar();
@@ -187,8 +176,4 @@ class _HomeState extends State<Home> {
       },
     );
   }
-
-
-
 }
-
